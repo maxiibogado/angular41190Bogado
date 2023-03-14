bcbc3b3fef25d844f955278f43eee804e5eb7e47
Maxi <mbogado@xubio.com>
2023-03-14 01:00:56 -0300

    mi Primer Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { MiPrimerComponenteComponent } from './mi-primer-componente/mi-primer-componente.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MiPrimerComponenteComponent
   ],
   imports: [
     BrowserModule
